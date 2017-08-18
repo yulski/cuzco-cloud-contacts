fbf03347fcc695388c5ed7183a2243e0367f31bb
yulski <julexmma@gmail.com>
2017-08-18 23:31:06 +0100

    added Spark hello world w/ jtwig

--- src/main/java/com/yulski/cuzco/Hello.java
+++ src/main/java/com/yulski/cuzco/Hello.java
@@ -1,9 +1,34 @@
 package com.yulski.cuzco;
 
+import spark.ModelAndView;
+import spark.template.jtwig.JtwigTemplateEngine;
+import spark.Request;
+import spark.Response;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
 public class Hello {
 
+    private static final JtwigTemplateEngine jtwigEngine = new JtwigTemplateEngine("/templates");
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        port(getPortNumber());
+        get("/", (Request request, Response response) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("projectName", "Cuzco");
+            return jtwigEngine.render(new ModelAndView(model, "index.twig"));
+        });
+    }
+
+    private static int getPortNumber() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
 
 }
