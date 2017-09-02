bece759dc85d889b88d37b2f880ef8cd2c520f1c
yulski <julexmma@gmail.com>
2017-09-02 01:44:10 +0100

    finished testing filters

--- src/test/java/com/yulski/cuzco/util/FiltersTest.java
+++ src/test/java/com/yulski/cuzco/util/FiltersTest.java
@@ -2,7 +2,9 @@ package com.yulski.cuzco.util;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.yulski.cuzco.models.Contact;
 import com.yulski.cuzco.models.User;
+import com.yulski.cuzco.services.ContactService;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import spark.*;
@@ -120,6 +122,144 @@ public class FiltersTest {
         expectFilterToTriggerForNonJsonRequest(mockRequest, mockResponse, notLoggedInFilter, redirectPath);
     }
 
+    @Test
+    public void contactAccessFilterValidReqiestShouldNotHaltTest() throws Exception {
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        SessionManager mockSessionManager = mock(SessionManager.class);
+        User mockUser = mock(User.class);
+        Contact mockContact = mock(Contact.class);
+        ContactService mockContactService = mock(ContactService.class);
+        final int contactId = 1;
+        final int userId = 100;
+
+        when(mockRequest.session()).thenReturn(mockSession);
+        when(mockRequest.params("id")).thenReturn(Integer.toString(contactId));
+        when(mockRequest.headers("Accept")).thenReturn("text/html");
+        when(mockSessionManager.getUser(mockSession)).thenReturn(mockUser);
+        when(mockUser.getId()).thenReturn(userId);
+        when(mockContact.getUser()).thenReturn(mockUser);
+        when(mockContactService.getOne(contactId)).thenReturn(mockContact);
+
+        final String redirectPath = "/test/path";
+        Filters filters = new Filters(mockSessionManager);
+        Filter contactAccessFilter = filters.contactAccess(redirectPath, mockContactService);
+
+        expectFilterToPass(mockRequest, mockResponse, contactAccessFilter);
+    }
+
+    @Test
+    public void contactAccessFilterContactDoesntExistJsonRequestShouldHaltWithStatusCode400AndEmptyJsonStringTest() {
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        SessionManager mockSessionManager = mock(SessionManager.class);
+        User mockUser = mock(User.class);
+        ContactService mockContactService = mock(ContactService.class);
+        final int contactId = 1;
+        final int userId = 100;
+
+        when(mockRequest.session()).thenReturn(mockSession);
+        when(mockRequest.params("id")).thenReturn(Integer.toString(contactId));
+        when(mockRequest.headers("Accept")).thenReturn("application/json");
+        when(mockSessionManager.getUser(mockSession)).thenReturn(mockUser);
+        when(mockUser.getId()).thenReturn(userId);
+        when(mockContactService.getOne(contactId)).thenReturn(null);
+
+        final String redirectPath = "/test/path";
+        Filters filters = new Filters(mockSessionManager);
+        Filter contactAccessFilter = filters.contactAccess(redirectPath, mockContactService);
+
+        expectFilterToTriggerForJsonRequest(mockRequest, mockResponse, contactAccessFilter);
+    }
+
+
+    @Test
+    public void contactAccessFilterContactDoesntExistNonJsonRequestShouldRedirectAndHaltWithStatusCode400Test() {
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        SessionManager mockSessionManager = mock(SessionManager.class);
+        User mockUser = mock(User.class);
+        ContactService mockContactService = mock(ContactService.class);
+        final int contactId = 1;
+        final int userId = 100;
+
+        when(mockRequest.session()).thenReturn(mockSession);
+        when(mockRequest.params("id")).thenReturn(Integer.toString(contactId));
+        when(mockRequest.headers("Accept")).thenReturn("text/html");
+        when(mockSessionManager.getUser(mockSession)).thenReturn(mockUser);
+        when(mockUser.getId()).thenReturn(userId);
+        when(mockContactService.getOne(contactId)).thenReturn(null);
+
+        final String redirectPath = "/test/path";
+        Filters filters = new Filters(mockSessionManager);
+        Filter contactAccessFilter = filters.contactAccess(redirectPath, mockContactService);
+
+        expectFilterToTriggerForNonJsonRequest(mockRequest, mockResponse, contactAccessFilter, redirectPath);
+    }
+
+    @Test
+    public void contactAccessFilterUserIsNotOwnerJsonRequestShouldHaltWithStatusCode400AndEmptyJsonStringTest() {
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        SessionManager mockSessionManager = mock(SessionManager.class);
+        Contact mockContact = mock(Contact.class);
+        User mockUser = mock(User.class);
+        User mockContactOwner = mock(User.class);
+        ContactService mockContactService = mock(ContactService.class);
+        final int contactId = 1;
+        final int userId = 100;
+        final int contactOwnerId = 999;
+
+        when(mockRequest.session()).thenReturn(mockSession);
+        when(mockRequest.params("id")).thenReturn(Integer.toString(contactId));
+        when(mockRequest.headers("Accept")).thenReturn("application/json");
+        when(mockSessionManager.getUser(mockSession)).thenReturn(mockUser);
+        when(mockUser.getId()).thenReturn(userId);
+        when(mockContactOwner.getId()).thenReturn(contactOwnerId);
+        when(mockContactService.getOne(contactId)).thenReturn(mockContact);
+        when(mockContact.getUser()).thenReturn(mockContactOwner);
+
+        final String redirectPath = "/test/path";
+        Filters filters = new Filters(mockSessionManager);
+        Filter contactAccessFilter = filters.contactAccess(redirectPath, mockContactService);
+
+        expectFilterToTriggerForJsonRequest(mockRequest, mockResponse, contactAccessFilter);
+    }
+
+    @Test
+    public void contactAccessFilterUserIsNotOwnerNonJsonRequestShouldRedirectAndHaltWithStatusCode400Test() {
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        SessionManager mockSessionManager = mock(SessionManager.class);
+        Contact mockContact = mock(Contact.class);
+        User mockUser = mock(User.class);
+        User mockContactOwner = mock(User.class);
+        ContactService mockContactService = mock(ContactService.class);
+        final int contactId = 1;
+        final int userId = 100;
+        final int contactOwnerId = 999;
+
+        when(mockRequest.session()).thenReturn(mockSession);
+        when(mockRequest.params("id")).thenReturn(Integer.toString(contactId));
+        when(mockRequest.headers("Accept")).thenReturn("text/html");
+        when(mockSessionManager.getUser(mockSession)).thenReturn(mockUser);
+        when(mockUser.getId()).thenReturn(userId);
+        when(mockContactOwner.getId()).thenReturn(contactOwnerId);
+        when(mockContactService.getOne(contactId)).thenReturn(mockContact);
+        when(mockContact.getUser()).thenReturn(mockContactOwner);
+
+        final String redirectPath = "/test/path";
+        Filters filters = new Filters(mockSessionManager);
+        Filter contactAccessFilter = filters.contactAccess(redirectPath, mockContactService);
+
+        expectFilterToTriggerForNonJsonRequest(mockRequest, mockResponse, contactAccessFilter, redirectPath);
+    }
+
     private void expectFilterToPass(Request mockRequest, Response mockResponse, Filter filter) throws Exception {
         try {
             filter.handle(mockRequest, mockResponse);
@@ -161,7 +301,5 @@ public class FiltersTest {
             assertEquals(400, haltException.statusCode());
         }
     }
-
-    // TODO test contact access filter
 
 }
